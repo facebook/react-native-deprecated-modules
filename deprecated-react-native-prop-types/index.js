5b731be749b8870e38927dcb53a3fd3a0b7439fb
Tim Yung <yungsters@fb.com>
2021-06-09 23:17:03 -0700

    Create 'deprecated-react-native-prop-types' package

---
+++ deprecated-react-native-prop-types/index.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+module.exports = {
+  get ColorPropType() {
+    return require('./DeprecatedColorPropType');
+  },
+  get EdgeInsetsPropType() {
+    return require('./DeprecatedEdgeInsetsPropType');
+  },
+  get ImagePropTypes() {
+    return require('./DeprecatedImagePropType');
+  },
+  get PointPropType() {
+    return require('./DeprecatedPointPropType');
+  },
+  get TextInputPropTypes() {
+    return require('./DeprecatedTextInputPropTypes');
+  },
+  get TextPropTypes() {
+    return require('./DeprecatedTextPropTypes');
+  },
+  get ViewPropTypes() {
+    return require('./DeprecatedViewPropTypes');
+  },
+};
